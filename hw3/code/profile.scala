2803d4b824ab6210571e388a7458c2b1bfff398e
Eric Mullen <emullen@meowth.(none)>
2013-05-25 14:12:17 -0700

    still writing

--- hw3/code/profile.scala
+++ hw3/code/profile.scala
@@ -1,6 +1,17 @@
 
 object profile {
   def instrBranches(cfg:CFG) {
-    
+    def addBranchCounter(b:Block) {
+      val body = b.instrsSSA.init
+
+      val branch = b.instrsSSA.last 
+      val instrBranch = branch match {
+        case Br(a) => //write me
+        case Blbc(a,b,c) =>
+        case Blbs(a,b,c) =>
+        case Ret(a) =>
+      }
+    }
+    cfg.list.foreach(addBranchCounter)
   }
 }
